847b3ad4a63cbb40544fedd07f95b119a8ab14be
shogo-matsumura <s.matsumura.cingroup2012@gmail.com>
2024-05-09 20:45:19 +0900

    “UpdateJsCode”

--- 2_4/script.js
+++ 2_4/script.js
@@ -1,10 +1,6 @@
 $(function () {
     $("#button").on("click", function () {
-        var inputValue = $("#value").val(); // テキストボックスの値を取得
-        if (inputValue === "1") {
-            alert("1");
-        } else if (inputValue === "2") {
-            alert("2");
-        }
+        let inputValue = $("#value").val(); // テキストボックスの値を取得
+        alert(inputValue); // 入力された値をアラートする
     });
 });
